a828745f14e183f7f202e3dea342910ba0c8b34a
Hope Dorkenoo <compounddork@gmail.com>
2022-05-12 13:20:30 +0100

    initial

---
+++ netlify-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+if cd flutter 
+then 
+    cd .. 
+else 
+    git clone --branch "2.10.5" https://github.com/flutter/flutter.git 
+fi
+
+flutter/bin/flutter config --enable-web
+flutter/bin/flutter pub get
+flutter/bin/flutter pub run build_runner build --delete-conflicting-outputs 
+flutter/bin/flutter build web --web-renderer html --release
