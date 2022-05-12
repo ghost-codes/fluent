3faa1937c23c4794888a63f779bd6eef39ab4a85
Hope Dorkenoo <compounddork@gmail.com>
2022-05-12 13:27:41 +0100

    k

--- netlify-build.sh
+++ netlify-build.sh
@@ -10,5 +10,5 @@ fi
 
 flutter/bin/flutter config --enable-web
 flutter/bin/flutter pub get
-flutter/bin/flutter pub run build_runner build --delete-conflicting-outputs 
+# flutter/bin/flutter pub run build_runner build --delete-conflicting-outputs 
 flutter/bin/flutter build web --web-renderer html --release
